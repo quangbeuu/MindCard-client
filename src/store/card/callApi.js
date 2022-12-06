6d0f2b40d0309e384b30d2efa1db31afe4d8b49b
quangvu0905 <97514754+Thombui@users.noreply.github.com>
2022-12-06 22:24:01 +0700

    a

--- src/store/card/callApi.js
+++ src/store/card/callApi.js
@@ -2,5 +2,6 @@ import axios from "axios";
 import { domain } from "../../utils/common";
 
 export default function requestGetAllCardInSet(setId = "") {
+  console.log("x");
   return axios.get(`${domain}/api/v1/sets/${setId}/getAllCard`);
 }
